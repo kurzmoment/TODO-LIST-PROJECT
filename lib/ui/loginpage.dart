413be4104ae8bf06daaf12d924a01b5ef5b51f39
kurzmoment <matej.kratochvil33@seznam.cz>
2021-03-15 00:24:05 +0100

    loginpage - skorohotova, chybi grafika, funkce

--- lib/ui/loginpage.dart
+++ lib/ui/loginpage.dart
@@ -1,13 +1,20 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:todoList/hexcolor.dart';
 
 class LoginPage extends StatelessWidget {
+  // Map<String, IconData> iconPack = {
+  //   'apple': FontAwesomeIcons.apple,
+  //   'google': FontAwesomeIcons.google,
+  //   'facebook': FontAwesomeIcons.facebook
+  // };
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: HexColor('#E5E5E5'),
+        backgroundColor: HexColor('#D5F3EF'),
         body: Container(
           child: Login(),
         ),
@@ -24,7 +31,7 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
         Center(
           child: Padding(
@@ -49,65 +56,136 @@ class _LoginState extends State<Login> {
             ),
           ),
         ),
+        inputText('Email'),
+        emailForm(),
+        inputText('Password'),
+        passwordForm(),
         Container(
           alignment: Alignment.centerLeft,
           child: Padding(
-            padding: const EdgeInsets.only(left: 15),
-            child: Text(
-              'Email',
-              style: TextStyle(fontSize: 15, color: Colors.blueAccent.shade200),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 15, right: 100),
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              border: Border.all(color: Colors.blue.shade100, width: 2),
-            ),
-            child: TextField(
-              decoration: InputDecoration(
-                border: InputBorder.none,
+            padding: const EdgeInsets.only(left: 190, top: 0),
+            child: TextButton(
+              child: Text(
+                'Forgot password?',
+                style: TextStyle(color: Colors.blueAccent.shade200),
               ),
+              onPressed: () => debugPrint('forgot password?'),
             ),
+          ),
+        ),
+        Row(
+          children: [
+            loginOptions('google', Colors.black),
+            loginOptions('apple', Colors.black),
+            loginOptions('facebook', Colors.blueAccent.shade700),
+          ],
+        ),
+        Container(
+          alignment: Alignment(-0.9, 0),
+          child: TextButton(
+            onPressed: () => debugPrint('New here? Register.'),
+            child: Text('New here? Register.'),
           ),
         ),
         Container(
-          alignment: Alignment.centerLeft,
+          width: 20,
           child: Padding(
-            padding: const EdgeInsets.only(left: 15, top: 10),
-            child: Text(
-              'Password',
-              style: TextStyle(fontSize: 15, color: Colors.blueAccent.shade200),
+            padding: const EdgeInsets.all(40),
+            child: ElevatedButton(
+              style: ButtonStyle(),
+              child: Text('Login'),
+              onPressed: () => debugPrint('login'),
             ),
           ),
+        )
+      ],
+    );
+  }
+}
+
+Widget inputText(String thing) {
+  return Container(
+    alignment: Alignment.centerLeft,
+    child: Padding(
+      padding: const EdgeInsets.only(left: 15, top: 10),
+      child: Text(
+        '${thing}',
+        style: TextStyle(fontSize: 15, color: Colors.blueAccent.shade200),
+      ),
+    ),
+  );
+}
+
+Widget emailForm() {
+  return Padding(
+    padding: const EdgeInsets.only(left: 15, right: 100),
+    child: Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(color: Colors.blue.shade100, width: 2),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          border: InputBorder.none,
         ),
-        Padding(
-          padding: const EdgeInsets.only(left: 15, right: 100),
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              border: Border.all(color: Colors.blue.shade100, width: 2),
-            ),
-            child: TextField(
-              decoration: InputDecoration(
-                border: InputBorder.none,
-              ),
-            ),
-          ),
-        ),
+      ),
+    ),
+  );
+}
+
+Widget passwordForm() {
+  return Padding(
+    padding: const EdgeInsets.only(left: 15, right: 100),
+    child: Column(
+      children: [
         Container(
-          alignment: Alignment.centerLeft,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 190, top: 5),
-            child: Text(
-              'Forgot password?',
-              style: TextStyle(fontSize: 15, color: Colors.blueAccent.shade200),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            border: Border.all(color: Colors.blue.shade100, width: 2),
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              suffixIcon: showPassword(),
             ),
           ),
         ),
       ],
-    );
-  }
+    ),
+  );
+}
+
+Widget showPassword() {
+  return IconButton(
+    icon: FaIcon(
+      FontAwesomeIcons.eye,
+      color: Colors.black38,
+    ),
+    onPressed: () => debugPrint('oko stisknuto'),
+  );
+}
+
+Widget loginOptions(String firma, Color color) {
+  Map<String, IconData> iconPack = {
+    'apple': FontAwesomeIcons.apple,
+    'google': FontAwesomeIcons.google,
+    'facebook': FontAwesomeIcons.facebook
+  };
+  return Padding(
+    padding: const EdgeInsets.only(left: 10),
+    child: Container(
+      margin: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+      ),
+      child: IconButton(
+        icon: FaIcon(
+          iconPack[firma],
+          color: color,
+        ),
+        onPressed: () => debugPrint(firma),
+      ),
+    ),
+  );
 }
