27f281266332e1c9524695b48f923332117f23ac
Pavel <pavelkurek@seznam.cz>
2021-03-16 17:30:28 +0100

    Firebase

--- lib/ui/addActivity.dart
+++ lib/ui/addActivity.dart
@@ -24,21 +24,22 @@ class AddActivity extends StatelessWidget {
     'green': Colors.green,
     'pink': Colors.pink
   };
-  final firebaseDB = Firestore.instance.collection('test').snapshots();
+  var firebaseDB = Firestore.instance.collection('test').snapshots();
   static var date = DateTime.now();
   final dateFormat = DateFormat('EEEE / dd.MM.yyyy').format(date);
 
   @override
   Widget build(BuildContext context) {
-    //var snapshotData = snapshot.documents[index].data;
-    //var docId = snapshot.documents[index].documentID;
+    var snapshotData = snapshot.documents[index].data;
+    var docId = snapshot.documents[index].documentID;
+
     return StreamBuilder(
       stream: firebaseDB,
       builder: (context, snapshot) {
         if (!snapshot.hasData) return CircularProgressIndicator();
         return ListView.builder(
           itemCount: snapshot.data.documents.length,
-          itemBuilder: (context, int index) {
+          itemBuilder: (context, int indexx) {
             return Slidable(
               actionPane: SlidableDrawerActionPane(),
               secondaryActions: [
@@ -57,16 +58,17 @@ class AddActivity extends StatelessWidget {
                   ),
                 ),
                 IconSlideAction(
-                  icon: Icons.delete,
-                  color: Colors.red,
-                  caption: 'Delete',
-                  // onTap: () async {
-                  //   await Firestore.instance
-                  //       .collection('test')
-                  //       .document(docId)
-                  //       .delete();
-                  // },
-                ),
+                    icon: Icons.delete,
+                    color: Colors.red,
+                    caption: 'Delete',
+                    onTap: () async {
+                      var collectionReference =
+                          Firestore.instance.collection("test");
+                      await collectionReference
+                          .document(docId)
+                          .delete()
+                          .catchError((error) => print("$error"));
+                    }),
               ],
               child: Card(
                 shadowColor: Colors.black,
@@ -96,84 +98,6 @@ class AddActivity extends StatelessWidget {
                 ),
               ),
             );
-            // return Padding(
-            //   padding: const EdgeInsets.all(8.0),
-            //   child: Container(
-            //     margin: EdgeInsets.only(right: 70),
-            //     decoration: BoxDecoration(
-            //       color: Colors.teal.shade50,
-            //       border: Border.all(
-            //         color: colorsMapping[snapshot.data.documents[index]
-            //             ['barva']],
-            //       ),
-            //       borderRadius: BorderRadius.circular(30),
-            //     ),
-            //     height: 50,
-            //     child: Stack(
-            //       children: [
-            //         Row(
-            //           crossAxisAlignment: CrossAxisAlignment.start,
-            //           children: [
-            //             Container(
-            //               margin:
-            //                   EdgeInsets.only(left: 10, top: 8, right: 15),
-            //               decoration: BoxDecoration(
-            //                 borderRadius: BorderRadius.circular(25),
-            //               ),
-            //               child: Padding(
-            //                 padding: const EdgeInsets.all(1),
-            //                 child: Icon(
-            //                   iconMapping[snapshot.data.documents[index]
-            //                       ['ikona']],
-            //                   size: 30,
-            //                   color: colorsMapping[
-            //                       snapshot.data.documents[index]['barva']],
-            //                 ),
-            //               ),
-            //             ),
-            //             Padding(
-            //               padding: const EdgeInsets.symmetric(horizontal: 5),
-            //               child: Column(
-            //                 crossAxisAlignment: CrossAxisAlignment.start,
-            //                 mainAxisAlignment: MainAxisAlignment.center,
-            //                 children: [
-            //                   Text(
-            //                     snapshot.data.documents[index]['title'],
-            //                     style: TextStyle(
-            //                       fontSize: 15,
-            //                       fontWeight: FontWeight.bold,
-            //                     ),
-            //                   ),
-            //                   Text(
-            //                     snapshot.data.documents[index]['name'],
-            //                     style: TextStyle(fontSize: 12),
-            //                   )
-            //                 ],
-            //               ),
-            //             ),
-            //           ],
-            //         ),
-            //         Column(
-            //           children: [
-            //             Padding(
-            //               padding: const EdgeInsets.only(top: 4, left: 265),
-            //               child: Container(
-            //                 margin: EdgeInsets.all(4),
-            //                 decoration: BoxDecoration(
-            //                     color: Colors.blueGrey,
-            //                     borderRadius: BorderRadius.circular(25)),
-            //                 child: Padding(
-            //                   padding: const EdgeInsets.all(4),
-            //                   child: Icon(Icons.edit),
-            //                 ),
-            //               ),
-            //             ),
-            //           ],
-            //         ),
-            //       ],
-            //     ),
-            //   ),
-            // );
           },
         );
       },
