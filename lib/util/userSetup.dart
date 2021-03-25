70fe616ce5c36edfda56a0c2a80cb34af29f635b
kurzmoment <68486535+kurzmoment@users.noreply.github.com>
2021-03-25 15:41:51 +0100

    vytvareni aktivity pro user

--- lib/util/userSetup.dart
+++ lib/util/userSetup.dart
@@ -2,9 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 Future<void> userSetup(String displayName) async {
-  CollectionReference users = FirebaseFirestore.instance.collection('Users');
   FirebaseAuth auth = FirebaseAuth.instance;
   String uid = auth.currentUser.uid.toString();
+  CollectionReference users = FirebaseFirestore.instance
+      .collection('userData')
+      .doc(uid)
+      .collection('activity');
   users.add({
     'displayName': displayName,
     'uid': uid,
