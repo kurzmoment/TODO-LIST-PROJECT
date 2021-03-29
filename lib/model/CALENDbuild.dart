1fe97839afb73adc99e95bedd8a6fd0114db295e
Pavel <pavelkurek@seznam.cz>
2021-03-29 16:59:35 +0200

    aktivita na kal

---
+++ lib/model/CALENDbuild.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:todoList/model/Activity_This_Week_Show.dart';
+
+class ActivityThisWeekBuilder extends StatelessWidget {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('userData')
+          .doc(auth.currentUser.uid)
+          .collection('activity')
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return CircularProgressIndicator();
+        return ListView.builder(
+          shrinkWrap: true,
+          itemCount: snapshot.data.docs.length,
+          itemBuilder: (context, int index) {
+            return ActivityThisWeekShow(snapshot: snapshot.data, index: index);
+          },
+        );
+      },
+    );
+  }
+}
