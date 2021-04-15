630d6769386b6de18ec095b4bf894e306f1a7cab
kurzmoment <68486535+kurzmoment@users.noreply.github.com>
2021-04-15 15:53:09 +0200

    edit  profile

--- lib/ui/editProfile.dart
+++ lib/ui/editProfile.dart
@@ -7,6 +7,14 @@ class EditProfilePage extends StatefulWidget {
 }
 
 class _EditProfilePageState extends State<EditProfilePage> {
+  _showModalBottomSheet(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container();
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,20 +32,68 @@ class _EditProfilePageState extends State<EditProfilePage> {
       body: ListView(
         children: [
           Card(
+            margin: EdgeInsets.fromLTRB(0, 1, 0, 1),
             child: ListTile(
               title: Text('Zmena uzivatelskeho jmena'),
+              trailing: IconButton(
+                icon: Icon(Icons.arrow_forward_ios),
+                onPressed: () {
+                  _showModalBottomSheet(context);
+                },
+              ),
             ),
           ),
           Card(
+            margin: EdgeInsets.fromLTRB(0, 1, 0, 1),
             child: ListTile(
               title: Text('Zmena emailu'),
+              trailing: IconButton(
+                icon: Icon(Icons.arrow_forward_ios),
+                onPressed: () {},
+              ),
             ),
           ),
           Card(
+            margin: EdgeInsets.fromLTRB(0, 1, 0, 1),
             child: ListTile(
               title: Text('Zmena hesla'),
+              trailing: IconButton(
+                icon: Icon(Icons.arrow_forward_ios),
+                onPressed: () {},
+              ),
             ),
-          )
+          ),
+          Card(
+            margin: EdgeInsets.fromLTRB(0, 1, 0, 1),
+            child: ListTile(
+              title: Text('Informace o uzivateli'),
+              trailing: IconButton(
+                icon: Icon(Icons.arrow_forward_ios),
+                onPressed: () {},
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 280),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image(
+                  image: AssetImage('assets/logo.png'),
+                  height: 60,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 4),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Version Alpha 0.5.0'),
+              ],
+            ),
+          ),
         ],
       ),
     );
